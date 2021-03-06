e4719d3dfa388cd8b71852d31efb8e676de974c4
camistyles <41596121+camistyles@users.noreply.github.com>
2021-03-06 12:22:28 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key
 
-# Hola a todos companeros, Carlos es el mejor profe de AWS del Mundo
+# Hola soy camilo como van
 
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
